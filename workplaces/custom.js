d5cbaee8db76f95cb6da6e7fe0b771a9f1d3cf64
knyazkova <100420736+knyazkova@users.noreply.github.com>
2022-04-28 16:21:04 +0300

    Add files via upload

---
+++ workplaces/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.number').spincrement({
+        thousandSeparator: "",
+        duration: 2500
+    });
+});
+$(document).ready(function(){
+  $('.title_big_rectlangle').spincrement({
+      thousandSeparator: "",
+      duration: 2000
+  });
+});
